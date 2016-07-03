cd99e7b18e11bbb304efa0c5c014fb6a938a65d7
NineMa <nine.exeo02@gmail.com>
2016-07-03 14:50:50 +0700

    create addUser.aspx
    It's fail.

--- RMTTS-2.0/pages/addUsers.aspx.cs
+++ RMTTS-2.0/pages/addUsers.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace RMTTS_2._0.pages
 {
-    public partial class addUser : System.Web.UI.Page
+    public partial class addUsers : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
